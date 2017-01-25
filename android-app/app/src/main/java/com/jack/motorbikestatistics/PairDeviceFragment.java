9c46e253e20e367c2b20b5ca52c6a4e9321b72eb
Jack Allister <b3042098@my.shu.ac.uk>
2017-01-25 20:06:26 +0000

    Discovery now works, needed to request permissions at run time

--- android-app/app/src/main/java/com/jack/motorbikestatistics/PairDeviceFragment.java
+++ android-app/app/src/main/java/com/jack/motorbikestatistics/PairDeviceFragment.java
@@ -1,43 +1,44 @@
 package com.jack.motorbikestatistics;
 
+import android.Manifest;
 import android.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 import android.widget.ListView;
 import java.util.Set;
 import java.util.ArrayList;
 import android.widget.Toast;
 import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
 import android.content.Intent;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-
 
 /**
  * Created by Jack on 25-Jan-17.
  */
 
-public class PairDeviceFragment extends Fragment
-        implements View.OnClickListener {
+public class PairDeviceFragment extends Fragment {
     private static int REQUEST_BLUETOOTH = 1;
 
     private View myView;
-    private Button btnScan;
-    private Button btnPair;
+    private ToggleButton btnScan;
     private ListView lvDevices;
 
     /* Bluetooth variables */
     private BluetoothAdapter btAdapter = null;
-    private Set<BluetoothDevice> pairedDevices;
 
+    private ArrayList<BTDeviceItem> btPairedItemList;
     private ArrayList<BTDeviceItem> btDeviceItemList;
     private ArrayAdapter<BTDeviceItem> lvAdapter;
 
@@ -47,15 +48,16 @@ public class PairDeviceFragment extends Fragment
 
         myView = inflater.inflate(R.layout.pairdevice_layout, container, false);
 
+        getNeededPrivileges();
+
         /* Get bluetooth adapter for device & create device array */
         btAdapter = BluetoothAdapter.getDefaultAdapter();
+        btPairedItemList = new ArrayList<BTDeviceItem>();
         btDeviceItemList = new ArrayList<BTDeviceItem>();
 
         /* Set our variables for UI buttons */
-        btnScan = (Button)myView.findViewById(R.id.btnScan);
-        btnScan.setOnClickListener(this);
-        btnPair = (Button)myView.findViewById(R.id.btnPair);
-        btnPair.setOnClickListener(this);
+        btnScan = (ToggleButton)myView.findViewById(R.id.btnScan);
+        btnScan.setOnCheckedChangeListener(toggleScanListener);
 
         lvDevices = (ListView)myView.findViewById(R.id.deviceList);
         lvAdapter = new BTDeviceListAdapter(getActivity(), R.layout.device_list_item, btDeviceItemList, btAdapter);
@@ -77,43 +79,92 @@ public class PairDeviceFragment extends Fragment
             }
 
             /* Add all paired devices to list */
-            pairedDevices = btAdapter.getBondedDevices();
+            Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
             if (pairedDevices.size() > 0)
             {
                 for (BluetoothDevice device : pairedDevices)
                 {
                     BTDeviceItem newDevice =
                             new BTDeviceItem(device.getName(), device.getAddress(), "paired", false);
-                    btDeviceItemList.add(newDevice);
+                    btPairedItemList.add(newDevice);
                 }
             }
             else if (pairedDevices.size() == 0)
             {
                 /* If device has no paired devices already */
-                btDeviceItemList.add(new BTDeviceItem("No Devices", "", "", false));
+                btPairedItemList.add(new BTDeviceItem("No Devices", "", "", false));
             }
+            btDeviceItemList.addAll(btPairedItemList);
 
         }
 
         return myView;
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId())
-        {
-            case R.id.btnScan:
-            {
-                Toast.makeText(getActivity().getApplicationContext(),
-                        "Button pressed", Toast.LENGTH_LONG).show();
-                break;
-            }
+    private void getNeededPrivileges()
+    {
+        final int REQUEST_CODE = 5;
 
-            case R.id.btnPair:
-            {
-                break;
-            }
+        boolean result = true;
+        boolean permsGranted = true;
+        String[] permsToRequest =
+                {
+                        Manifest.permission.BLUETOOTH_ADMIN,
+                        Manifest.permission.BLUETOOTH,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                };
+
+        for (String permission: permsToRequest)
+        {
+            permsGranted &= (ContextCompat.checkSelfPermission(getActivity(), permission) == PackageManager.PERMISSION_GRANTED);
+        }
+
+        if (permsGranted == false)
+        {
+            ActivityCompat.requestPermissions(getActivity(), permsToRequest, REQUEST_CODE);
         }
     }
+
+    public final BroadcastReceiver btReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            /* Check to see if found device */
+            if (BluetoothDevice.ACTION_FOUND.equals(action))
+            {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                /* Create new device item and add to list */
+                BTDeviceItem newDevice = new BTDeviceItem(device.getName(),
+                                                device.getAddress(), "unpaired", false);
+                lvAdapter.add(newDevice);
+                lvAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+    public final ToggleButton.OnCheckedChangeListener toggleScanListener = new ToggleButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            if (isChecked == true)
+            {
+                /* Clear listview, add previous paired items, start discovery */
+                lvAdapter.clear();
+                lvAdapter.addAll(btPairedItemList);
+                getActivity().registerReceiver(btReceiver, filter);
+                btAdapter.startDiscovery();
+            }
+            else
+            {
+                /* Stop searching for new devices */
+                getActivity().unregisterReceiver(btReceiver);
+                btAdapter.cancelDiscovery();
+            }
+        }
+    };
 
 }
